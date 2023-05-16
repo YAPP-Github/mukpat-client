cd684ad7b397cf03f34a3ebba7ce5449c131914f
Yangseungchan <uvzone@naver.com>
2023-05-16 21:29:34 +0900

    chore: yarn zero-install 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body>
         {children}
       </body>
