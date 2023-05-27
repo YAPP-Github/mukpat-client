c229ecc47cdde8fbb7e245a4d1d1a7628e5c2af7
Yangseungchan <uvzone@naver.com>
2023-05-27 17:48:29 +0900

    chore: 폴더 구조 변경사항 반영

---
+++ src/app/post/components/index.ts
@@ -0,0 +1,3 @@
+export { default as PostClientComponent } from './Post/Post';
+
+export { default as PostServerComponent } from './Post/Post.server';
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,2 @@
 export { default as Badge } from './Badge/Badge';
-export { default as Post } from './Post/Post';
+export { default as Post } from '../app/post/components/Post/Post';
