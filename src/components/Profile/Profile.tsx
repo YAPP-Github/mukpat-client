cea8c8b109ad63c1ecf78e603edca9042678c9a3
naro_Kim <gracias9022@gmail.com>
2023-05-29 18:22:36 +0900

    feat: uid color mapping 방식 변경

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -5,11 +5,10 @@ import { type ProfileVariants, wrapper } from './Profile.css';
 type ProfileProps = { uid: number; nickname: string } & ProfileVariants;
 
 const Profile = ({ uid, nickname, size = 'small', ...rest }: ProfileProps) => {
-	const colorVariant =
-		uid % 5 === 0 ? 'purple' : uid % 5 === 1 ? 'pink' : uid % 5 === 2 ? 'orange' : uid % 5 === 3 ? 'yellow' : 'skyblue';
+	const colorMap: Array<ProfileProps['color']> = ['purple', 'pink', 'orange', 'yellow', 'skyblue'];
 	return (
-		<div className={cx(wrapper({ color: colorVariant, size }))} {...rest} data-testid="profile">
-			{nickname.split(' ')[0][0].toUpperCase()}
+		<div className={cx(wrapper({ color: colorMap[uid % 5], size }))} {...rest} data-testid="profile">
+			{nickname.trim()[0].toUpperCase()}
 		</div>
 	);
 };
