552bd1855f7e61705a56ee337b3c31c7f69b6a14
Yangseungchan <uvzone@naver.com>
2023-05-30 00:15:24 +0900

    chore: Chip props 타입 변경

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -2,14 +2,27 @@ import { HTMLAttributes, ReactNode } from 'react';
 import clsx from 'classnames';
 import { wrapper, content, ChipWrapperVariants, ChipContentVariants } from './Chip.css';
 
-type Props = ChipWrapperVariants &
-	ChipContentVariants & {
-		children: ReactNode;
-		leftIcon?: ReactNode;
-		rightIcon?: ReactNode;
-	} & HTMLAttributes<HTMLDivElement>;
+type DivElement = Omit<HTMLAttributes<HTMLDivElement>, 'className'>;
+type ChipVariants = ChipWrapperVariants & ChipContentVariants & DivElement;
 
-const Chip = ({ color = 'grey', size = 'md', children, leftIcon, rightIcon, className, ...rest }: Props) => {
+type Props<Variants extends HTMLAttributes<HTMLDivElement>> = {
+	[key in keyof Variants]: Variants[key];
+} & {
+	children: ReactNode;
+	leftIcon?: ReactNode;
+	rightIcon?: ReactNode;
+	className?: string;
+};
+
+const Chip = ({
+	color = 'grey',
+	size = 'md',
+	children,
+	leftIcon,
+	rightIcon,
+	className = '',
+	...rest
+}: Props<ChipVariants>) => {
 	return (
 		<div className={clsx(wrapper({ color }), className)} {...rest}>
 			{leftIcon}
