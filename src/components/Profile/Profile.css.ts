343ee509ad42775f7b4be5e766d8b0b4171d4471
naro_Kim <gracias9022@gmail.com>
2023-05-31 02:08:44 +0900

    chore: Profile logic 변경

--- src/components/Profile/Profile.css.ts
+++ src/components/Profile/Profile.css.ts
@@ -3,6 +3,19 @@ import { recipe, RecipeVariants } from '@vanilla-extract/recipes';
 
 const { fontSize, fontWeight, lineHeights, borderRadius, color } = themeTokens;
 
+export const colorVariant = {
+	purple: { background: color.purple500, color: color.purple800 },
+	pink: { background: color.pink500, color: color.pink800 },
+	orange: { background: color.orange500, color: color.orange800 },
+	yellow: { background: color.yellow500, color: color.yellow800 },
+	skyblue: { background: color.skyblue500, color: color.skyblue800 },
+};
+
+export const sizeVariant = {
+	small: { width: 40, height: 40 },
+	medium: { width: 48, height: 48 },
+};
+
 export const wrapper = recipe({
 	base: {
 		display: 'flex',
@@ -14,23 +27,8 @@ export const wrapper = recipe({
 		borderRadius: borderRadius['circle'],
 	},
 	variants: {
-		color: {
-			purple: { background: color.purple500, color: color.purple800 },
-			pink: { background: color.pink500, color: color.pink800 },
-			orange: { background: color.orange500, color: color.orange800 },
-			yellow: { background: color.yellow500, color: color.yellow800 },
-			skyblue: { background: color.skyblue500, color: color.skyblue800 },
-		},
-		size: {
-			small: {
-				width: 40,
-				height: 40,
-			},
-			medium: {
-				width: 48,
-				height: 48,
-			},
-		},
+		color: colorVariant,
+		size: sizeVariant,
 	},
 	defaultVariants: {
 		color: 'purple',
@@ -38,4 +36,5 @@ export const wrapper = recipe({
 	},
 });
 
-export type ProfileVariants = RecipeVariants<typeof wrapper>;
+export type ProfileColor = NonNullable<RecipeVariants<typeof wrapper>>['color'];
+export type ProfileSize = NonNullable<RecipeVariants<typeof wrapper>>['size'];
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,13 +1,27 @@
 import cx from 'classnames';
 
-import { type ProfileVariants, wrapper } from './Profile.css';
+import { ProfileColor, ProfileSize, colorVariant, wrapper } from './Profile.css';
 
-type ProfileProps = { uid: number; nickname: string } & ProfileVariants;
+/**
+ * 프로필 공통 컴포넌트
+ * @property {number} uid - 유저 고유 id
+ * @property {string} nickname - 유저 고유 닉네임
+ * @property {ProfileColor} color? - 프로필 컬러
+ * @property {ProfileSize} size? - 프로필 사이즈
+ */
+type ProfileProps = {
+	uid: number;
+	nickname: string;
+	color?: ProfileColor;
+	size?: ProfileSize;
+};
 
-const Profile = ({ uid, nickname, size = 'small', ...rest }: ProfileProps) => {
-	const colorMap: Array<ProfileProps['color']> = ['purple', 'pink', 'orange', 'yellow', 'skyblue'];
+const Profile = ({ uid, nickname, color, size, ...rest }: ProfileProps) => {
+	const typedKeys = Object.keys as <T>(obj: T) => Array<keyof T>;
+	const colors = typedKeys(colorVariant);
+	color = color ? color : colors[uid % colors.length];
 	return (
-		<div className={cx(wrapper({ color: colorMap[uid % 5], size }))} {...rest} data-testid="profile">
+		<div {...rest} className={cx(wrapper({ color, size }))} data-testid="profile">
 			{nickname.trim()[0].toUpperCase()}
 		</div>
 	);
