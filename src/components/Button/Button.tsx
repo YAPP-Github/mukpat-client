60d9dff5dc3ea325eff2b1b0b2630395b7698ada
zzieun22 <36434665+jieunpark247@users.noreply.github.com>
2023-05-30 23:21:02 +0900

    edit: type ButtonProps로 변경

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,12 +2,12 @@ import { type Color, Size, buttonWrapper } from './Button.css';
 import { ButtonHTMLAttributes } from 'react';
 import cx from 'classnames';
 
-type Button = {
+type ButtonProps = {
 	color: Color;
 	size: Size;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button = ({ children = 'button', size, color, disabled, onClick, className, ...rest }: Button) => {
+const Button = ({ children = 'button', size, color, disabled, onClick, className, ...rest }: ButtonProps) => {
 	return (
 		<button {...rest} className={cx(buttonWrapper({ color, size }), className)} disabled={disabled} onClick={onClick}>
 			<div>{children}</div>
