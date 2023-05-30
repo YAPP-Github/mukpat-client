79a3aafb7d02d9686525ae17df8c298ea3768811
zzieun22 <36434665+jieunpark247@users.noreply.github.com>
2023-05-30 21:18:09 +0900

    edit: 버튼 타입 변경, onClick disable 분기처리 삭제

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,21 +1,15 @@
 import { type Color, Size, buttonWrapper } from './Button.css';
-import { HTMLAttributes } from 'react';
+import { ButtonHTMLAttributes } from 'react';
 import cx from 'classnames';
 
 type Button = {
-	disabled?: boolean;
 	color: Color;
 	size: Size;
-} & HTMLAttributes<HTMLButtonElement>;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button = ({ children = 'button', size, color, disabled, onClick, className, ...rest }: Button) => {
 	return (
-		<button
-			{...rest}
-			className={cx(buttonWrapper({ color, size }), className)}
-			disabled={disabled}
-			onClick={disabled ? undefined : onClick}
-		>
+		<button {...rest} className={cx(buttonWrapper({ color, size }), className)} disabled={disabled} onClick={onClick}>
 			<div>{children}</div>
 		</button>
 	);
