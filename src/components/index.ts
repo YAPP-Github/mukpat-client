0d845e60bb6a3e9edae63066a311b3455f94693e
zzieun22 <36434665+jieunpark247@users.noreply.github.com>
2023-05-31 00:15:58 +0900

    edit: IconButton 컴포넌트 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,5 @@ export { default as Badge } from './Badge/Badge';
 export { default as Typography } from './Typography/Typography';
 
 export { default as Button } from './Button/Button';
+
+export { default as IconButton } from './IconButton/IconButton';
