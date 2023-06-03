501c4283d4fcbbe64165f82677b7d220776f1468
Yangseungchan <uvzone@naver.com>
2023-06-03 15:02:38 +0900

    refactor: useClickoutside props 수정

--- src/hooks/useClickOutside/useClickOutside.ts
+++ src/hooks/useClickOutside/useClickOutside.ts
@@ -1,23 +1,44 @@
 import { useEffect, useRef } from 'react';
 
+type ClickOutsideEvents = Pick<
+	WindowEventMap,
+	'mousedown' | 'mouseup' | 'touchstart' | 'touchend' | 'pointerdown' | 'pointerup'
+>;
+
+/**
+ * @property {() => void} onClickOutside - 외부 클릭 이벤트 발생 시 실행할 함수
+ * @property {keyof ClickOutsideEvents} event - 외부 클릭 이벤트 종류
+ */
+
 interface Props {
 	onClickOutside: () => void;
+	event?: keyof ClickOutsideEvents;
 }
 
-const useClickOutside = <T extends HTMLElement = HTMLElement>({ onClickOutside }: Props) => {
+/**
+ * @param {Props} props
+ * @param {() => void} props.onClickOutside - 외부 클릭 이벤트 발생 시 실행할 함수
+ * @param {keyof ClickOutsideEvents} props.event - 외부 클릭 이벤트 종류
+ * @returns {React.RefObject<T>} 외부 클릭 이벤트를 감지할 element ref
+ */
+
+const useClickOutside = <T extends HTMLElement = HTMLElement>({
+	onClickOutside,
+	event = 'mousedown',
+}: Props): React.RefObject<T> => {
 	const ref = useRef<T>(null);
 
 	useEffect(() => {
-		const handleClickOutside = (event: MouseEvent) => {
-			if (ref.current && !ref.current.contains(event.target as Node)) {
+		const handleClickOutside = (e: ClickOutsideEvents[typeof event]) => {
+			if (ref.current && !ref.current.contains(e.target as Node)) {
 				onClickOutside();
 			}
 		};
-		window.addEventListener('mousedown', handleClickOutside);
+		window.addEventListener(event, handleClickOutside);
 		return () => {
-			window.removeEventListener('mousedown', handleClickOutside);
+			window.removeEventListener(event, handleClickOutside);
 		};
-	}, [onClickOutside]);
+	}, [onClickOutside, event]);
 
 	return ref;
 };
