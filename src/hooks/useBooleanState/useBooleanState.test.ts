9049871bbe8ef6d91a2f467c3390af1bc02828b4
Yangseungchan <uvzone@naver.com>
2023-06-05 23:39:38 +0900

    chore: 불필요한 테스트코드 제거

--- src/hooks/useBooleanState/useBooleanState.test.ts
+++ src/hooks/useBooleanState/useBooleanState.test.ts
@@ -3,18 +3,6 @@ import useBooleanState from './useBooleanState';
 import { act, renderHook } from '@/tests/test-utils';
 
 describe('useBooleanState 테스트', () => {
-	test('기본 초기 상태 값은 false다', () => {
-		const { result } = renderHook(() => useBooleanState());
-		const [bool] = result.current;
-		expect(bool).toBe(false);
-	});
-
-	test('인자값이 true면 초기 상태 값은 true다', () => {
-		const { result } = renderHook(() => useBooleanState(true));
-		const [bool] = result.current;
-		expect(bool).toBe(true);
-	});
-
 	test('setTrue 함수를 실행하면 true, setFalse 함수를 실행하면 false 된다.', () => {
 		const { result } = renderHook(() => useBooleanState());
 		const [, setTrue, setFalse] = result.current;
