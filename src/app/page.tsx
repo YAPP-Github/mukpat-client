a603cbd5066d1950ae3d709ac69270f0ca6c0a3b
Yangseungchan <uvzone@naver.com>
2023-06-05 19:28:39 +0900

    feat: sprite icon 생성 명령어 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { SvgIcon } from '@/components';
+
 export default function Home() {
 	return (
 		<div>
 			<div>home</div>
+			<SvgIcon id="calendar" />
+			<SvgIcon id="chevronleft" width={32} height={32} />
 		</div>
 	);
 }
