d4070e6b0d7a149543fcbee036badbf56a642e8f
naro_Kim <gracias9022@gmail.com>
2023-06-07 04:07:45 +0900

    feat: Input 컴포넌트 props 수정

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,27 +3,26 @@
 import Image from 'next/image';
 import { inputWrapper, input, Size, clearButton } from './Input.css';
 import { ForwardedRef, forwardRef } from 'react';
-import Typography from '../Typography/Typography';
-import { SchemaKeys } from './schema';
 import { getIconUrl } from '../IconButton/utils/getIconUrl';
 
 export type InputProps = {
-	name: SchemaKeys;
-	label?: string;
+	name: string;
 	size?: Size;
 } & React.ComponentPropsWithoutRef<'input'>;
 
 const Input = forwardRef(function Input(props: InputProps, ref: ForwardedRef<HTMLInputElement>) {
-	const { label, name, placeholder, size, ...rest } = props;
+	const { name, placeholder, size, ...rest } = props;
 	return (
-		<div className={inputWrapper} id={`${name}_input`}>
-			{label && (
-				<Typography variant="label3" as="label">
-					{label}
-				</Typography>
-			)}
-			<input name={name} className={input({ size })} placeholder={placeholder} ref={ref} {...rest}></input>
-			<button className={clearButton} type="reset">
+		<div className={inputWrapper} aria-describedby={`${name} input wrapper`} id={`${name}_input`}>
+			<input
+				name={name}
+				className={input({ size })}
+				placeholder={placeholder}
+				ref={ref}
+				aria-describedby={`${name} input field`}
+				{...rest}
+			></input>
+			<button className={clearButton} aria-label="Clear input" type="reset">
 				<Image src={getIconUrl('clear', 'default')} alt={'Icon'} width={24} height={24} />
 			</button>
 		</div>
