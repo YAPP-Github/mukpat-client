11fbe8fe248740932e97f7eb68af189342f2902b
naro_Kim <gracias9022@gmail.com>
2023-06-06 19:41:44 +0900

    refactor: Input 공통 컴포넌트 useController 변경

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,28 +1,31 @@
 'use client';
 
-import cx from 'classnames';
-import { RegisterOptions } from 'react-hook-form';
+import { useController, FieldValues, FieldPath, Control, RegisterOptions } from 'react-hook-form';
 import { inputWrapper, inputContainer, Size } from './Input.css';
 import { ForwardedRef, forwardRef } from 'react';
+import { IconType } from '../IconButton/utils/getIconUrl';
+import IconButton from '../IconButton/IconButton';
 import Typography from '../Typography/Typography';
-import { SchemaKeys } from './schema';
 
-export type InputProps = {
-	name: SchemaKeys;
-	label: string;
-	helperText?: string;
-	validation?: RegisterOptions;
+type TControl<T extends FieldValues> = {
+	control: Control<T>;
+	name: FieldPath<T>;
+	placeholder: string;
+	icons: IconType;
 	size?: Size;
 } & React.ComponentPropsWithoutRef<'input'>;
 
-const Input = forwardRef(function Input(props: InputProps, ref: ForwardedRef<HTMLInputElement>) {
-	const { label, name, placeholder, size, ...rest } = props;
+const Input = forwardRef(function Input(props: TControl<any>, ref: ForwardedRef<HTMLInputElement>) {
+	const { control, name, placeholder, icons, size } = props;
+	const { field, fieldState } = useController({ name, control });
+
 	return (
-		<div className={inputContainer({ size })} id={`${name}_input`}>
-			<Typography variant="label3" as="label">
-				{label}
+		<div className={inputContainer({ size })}>
+			<input {...field} className={inputWrapper({ size })} placeholder={placeholder} name={name} ref={ref}></input>
+			<IconButton iconType={icons}></IconButton>
+			<Typography color="red500" variant="label5" as="p">
+				{fieldState.error?.message}
 			</Typography>
-			<input name={name} className={inputWrapper({ size })} placeholder={placeholder} ref={ref} {...rest}></input>
 		</div>
 	);
 });
