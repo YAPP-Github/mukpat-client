7f1c866a1198b2ab650c9984ae17a618df387ade
naro_Kim <gracias9022@gmail.com>
2023-06-09 05:31:02 +0900

    feat: Controllable input 공통 컴포넌트 기능 요구사항 반영

--- src/components/Input/ControllerInput.tsx
+++ src/components/Input/ControllerInput.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import { useController, FieldValues, FieldPath, Control, Controller, useForm } from 'react-hook-form';
-import { inputWrapper, input, Size, clearButton } from './Input.css';
-import { ForwardedRef, forwardRef } from 'react';
+import clsx from 'classnames';
+import { useController, FieldValues, FieldPath, Control, Controller, useFormContext } from 'react-hook-form';
+import { inputWrapper, inputBase, Type, Size, clearButton, inputError } from './Input.css';
+import { ForwardedRef, forwardRef, useCallback } from 'react';
 import Image from 'next/image';
 import Typography from '../Typography/Typography';
 import { getIconUrl } from '../IconButton/utils/getIconUrl';
@@ -10,49 +11,45 @@ import { getIconUrl } from '../IconButton/utils/getIconUrl';
 type TControl<T extends FieldValues> = {
 	control: Control<T>;
 	name: FieldPath<T>;
-	placeholder: string;
 	size?: Size;
+	type?: Type;
 } & React.ComponentPropsWithoutRef<'input'>;
 
 const ControllerInput = forwardRef(function Input(props: TControl<any>, ref: ForwardedRef<HTMLInputElement>) {
-	const { control, name, placeholder, size } = props;
+	const { type, control, name, placeholder = '텍스트를 입력해주세요', size } = props;
+	const { resetField } = useFormContext();
 	const {
 		field: { value, ...rest },
 		fieldState,
 	} = useController({ name, control });
-	const { setValue } = useForm();
-	const clear = (name: string) => {
-		setValue(name, '', {
-			shouldValidate: true,
-			shouldDirty: true,
-		});
-	};
+	const handleReset = useCallback(() => resetField(name, { defaultValue: '', keepDirty: false }), []);
 
 	return (
-		<div className={inputWrapper}>
-			<Controller
-				control={control}
-				defaultValue=""
-				name={name}
-				render={({ field }) => (
-					<input {...rest} className={input({ size })} placeholder={placeholder} value={field.value} ref={ref} />
-				)}
-			/>
-			<button
-				className={clearButton}
-				disabled={!fieldState.isDirty}
-				type="reset"
-				onClick={() => {
-					console.log('clear', name);
-					clear(name);
-				}}
-			>
-				<Image src={getIconUrl('clear', 'default')} alt={'Icon'} width={24} height={24} />
-			</button>
+		<>
+			<div className={inputWrapper}>
+				<Controller
+					defaultValue=""
+					name={name}
+					render={({ field }) => (
+						<input
+							{...rest}
+							aria-invalid={fieldState.invalid}
+							className={clsx(inputBase({ size, type }), fieldState.error && inputError)}
+							placeholder={placeholder}
+							value={field.value}
+							type={type}
+							ref={ref}
+						/>
+					)}
+				/>
+				<button className={clearButton} onClick={handleReset} disabled={!fieldState.isDirty} type="button">
+					<Image src={getIconUrl('clear', 'default')} alt={'Icon'} width={24} height={24} />
+				</button>
+			</div>
 			<Typography color="red500" variant="label5" as="p">
 				{fieldState.error?.message}
 			</Typography>
-		</div>
+		</>
 	);
 });
 
