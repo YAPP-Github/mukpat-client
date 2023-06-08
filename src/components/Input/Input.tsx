39b939db33e0212bf1e0a725aa79284ca4589595
naro_Kim <gracias9022@gmail.com>
2023-06-09 03:04:45 +0900

    feat: input 공통 컴포넌트 요구사항 구현

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import Image from 'next/image';
-import { inputWrapper, input, Size, clearButton } from './Input.css';
-import { ForwardedRef, forwardRef } from 'react';
+import { inputWrapper, inputBase, Size, clearButton } from './Input.css';
+import { ForwardedRef, forwardRef, useCallback } from 'react';
+import { useFormContext } from 'react-hook-form';
 import { getIconUrl } from '../IconButton/utils/getIconUrl';
+import Typography from '../Typography/Typography';
 
 export type InputProps = {
 	name: string;
@@ -12,20 +14,33 @@ export type InputProps = {
 
 const Input = forwardRef(function Input(props: InputProps, ref: ForwardedRef<HTMLInputElement>) {
 	const { name, placeholder, size, ...rest } = props;
+	const { formState, resetField } = useFormContext();
+	const handleReset = useCallback(() => resetField(name, { defaultValue: '', keepDirty: false }), []);
+	const errorMessage = formState.errors[name]?.message as string;
+
 	return (
-		<div className={inputWrapper} aria-describedby={`${name} input wrapper`} id={`${name}_input`}>
-			<input
-				name={name}
-				className={input({ size })}
-				placeholder={placeholder}
-				ref={ref}
-				aria-describedby={`${name} input field`}
-				{...rest}
-			></input>
-			<button className={clearButton} aria-label="Clear input" type="reset">
-				<Image src={getIconUrl('clear', 'default')} alt={'Icon'} width={24} height={24} />
-			</button>
-		</div>
+		<>
+			<div className={inputWrapper} aria-describedby={`${name} input wrapper`} id={`${name}_input`}>
+				<input
+					name={name}
+					className={inputBase({ size })}
+					placeholder={placeholder}
+					ref={ref}
+					aria-describedby={`${name} input field`}
+					{...rest}
+				></input>
+				{formState.dirtyFields[name] && (
+					<button className={clearButton} onClick={handleReset} aria-describedby={`Clear ${name} input`} type="button">
+						<Image src={getIconUrl('clear', 'default')} alt={'Icon'} width={24} height={24} />
+					</button>
+				)}
+			</div>
+			{formState.errors[name]?.message && (
+				<Typography color="red500" variant="label5" as="p">
+					{errorMessage}
+				</Typography>
+			)}
+		</>
 	);
 });
 
