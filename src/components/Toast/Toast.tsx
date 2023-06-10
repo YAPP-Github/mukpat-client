c8d3cb87c088af5839fa7e3c524ca554661972a4
Yangseungchan <uvzone@naver.com>
2023-06-10 14:18:13 +0900

    feat: 토스트 타입 설정 기능 추가, 아이콘 변경

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,12 +1,17 @@
 'use client';
 
 import { motion } from 'framer-motion';
+import Image from 'next/image';
 import { HTMLAttributes, useEffect } from 'react';
 import { Typography } from '@/components';
-import ToastIcon from './ToastIcon';
 import { wrapper } from './Toast.css';
+import { getIconUrl } from '../IconButton/utils/getIconUrl';
+
+// TODO : type에 따라 다른 아이콘 구성하기 : warn, success
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
+	/** 토스트 종류 */
+	type: 'success' | 'warn';
 	/** 토스트 메시지 */
 	message: string;
 	/** 토스트가 닫힐때 호출되는 함수 */
@@ -15,7 +20,7 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
 	closeAfter?: number;
 }
 
-const Toast = ({ message, onClose, closeAfter = 2 }: Props) => {
+const Toast = ({ type, message, onClose, closeAfter = 2 }: Props) => {
 	useEffect(() => {
 		const timer = setTimeout(() => {
 			onClose();
@@ -28,7 +33,7 @@ const Toast = ({ message, onClose, closeAfter = 2 }: Props) => {
 
 	return (
 		<motion.div className={wrapper} initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-			<ToastIcon />
+			<Image src={getIconUrl('toast', type)} alt="" width={24} height={24} />
 			<Typography variant="body2" as="span" color="white">
 				{message}
 			</Typography>
