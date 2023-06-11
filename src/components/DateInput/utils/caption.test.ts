3dba920c72982de3da23aef4e326e3fcb1800b53
Yangseungchan <uvzone@naver.com>
2023-06-11 21:28:42 +0900

    chore: caption 유틸 함수 구성

---
+++ src/components/DateInput/utils/caption.test.ts
@@ -0,0 +1,19 @@
+import { yearMonthCaption, selectedDateCaption } from './caption';
+
+describe('DateInput caption 테스트', () => {
+	test('yearMonthCaption 테스트', () => {
+		const date = new Date('2023-06-01');
+		expect(yearMonthCaption(date)).toBe('2023.06');
+	});
+
+	test('selectedDateCaption 테스트', () => {
+		const date1 = new Date('2023-06-01');
+		expect(selectedDateCaption(date1)).toBe('2023년 06월 01일');
+
+		const today = new Date();
+		expect(selectedDateCaption(today)).toMatch(/(오늘)/i);
+
+		const tomorrow = new Date(today.getTime() + 24 * 60 * 60 * 1000);
+		expect(selectedDateCaption(tomorrow)).toMatch(/(내일)/i);
+	});
+});
