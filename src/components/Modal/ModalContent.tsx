4c91f3c906bf3a3ff87b84ab8d733ec17869fce9
zieun <jieunpark247@gmail.com>
2023-06-12 21:41:13 +0900

    feat: ModalContent 컴포넌트 추가

---
+++ src/components/Modal/ModalContent.tsx
@@ -0,0 +1,17 @@
+import { HTMLAttributes } from 'react';
+import cx from 'classnames';
+import { type ContentSize, contentWrapper } from './Modal.css';
+
+type ModalContentProps = {
+	size: ContentSize;
+} & HTMLAttributes<HTMLDivElement>;
+
+const ModalContent = ({ size, className, children, ...rest }: ModalContentProps) => {
+	return (
+		<div className={cx(contentWrapper({ size }), className)} {...rest}>
+			{children}
+		</div>
+	);
+};
+
+export default ModalContent;
