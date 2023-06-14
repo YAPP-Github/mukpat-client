a42ea71ce7726d0d86b1c7179511e3be9c895b1f
Yangseungchan <uvzone@naver.com>
2023-06-14 21:00:48 +0900

    chore: 프록시 설정 사항 수정

--- next.config.js
+++ next.config.js
@@ -11,15 +11,13 @@ const nextConfig = {
   // 프록시 설정
   async rewrites(){
     const API_ENDPOINT = process.env.API_URL ?? process.env.NEXT_PUBLIC_API_URL;
-    
-    if(process.env.NODE_ENV === 'development'){
-      return [
-        {
-          source: '/api/:path*',
-          destination: `${API_ENDPOINT}/:path*`, 
-        }
-      ]
-    }
+    return process.env.NODE_ENV === 'development' ? [
+      {
+        source: '/api/:path*',
+        destination: `${API_ENDPOINT}/:path*`, 
+      }
+    ] : [];
+  
   }
 }
 
