ac73c93ad376e82136209b38e04600c7a1c5f624
zieun <jieunpark247@gmail.com>
2023-06-17 21:02:50 +0900

    feat: map 사용 시 필요한 유틸 추가

---
+++ src/components/Map/utils/mapUtils.ts
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+interface LatLng {
+  La: number;
+  Ma: number;
+}
+export const createMap = (mapContainer: HTMLElement | null, location: LatLng) => {
+  const mapOption = {
+    center: location,
+    level: 3,
+  };
+  return new window.kakao.maps.Map(mapContainer, mapOption);
+};
+export const createMarker = (markerInfo: { position: GeolocationPosition } | null) => {
+  return new window.kakao.maps.Marker(markerInfo);
+};
+export const createLatLngBounds = () => {
+  return new window.kakao.maps.LatLngBounds();
+};
+export const createLatLng = ({ latitude, longitude }: { latitude: string; longitude: string }) => {
+  return new window.kakao.maps.LatLng(latitude, longitude);
+};
+export const creatPlaces = () => {
+  return new window.kakao.maps.services.Places();
+};
+export const creatGeocoder = () => {
+  return new window.kakao.maps.services.Geocoder();
+};
+export const getCurrentCoordinate = () => {
+  return new Promise<LatLng>((resolve, reject) => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        const location = new window.kakao.maps.LatLng(latitude, longitude);
+        resolve(location);
+      });
+    } else {
+      reject(new Error('Failed to get current position'));
+    }
+  });
+};
