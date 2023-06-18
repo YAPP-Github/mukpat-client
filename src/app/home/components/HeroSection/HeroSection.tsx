50d8e6e035e317ae9ad14247de118c7dc15df965
Yangseungchan <uvzone@naver.com>
2023-06-18 10:06:32 +0900

    feat: 메인 페이지 HeroSection 구성

---
+++ src/app/home/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import { Button, Typography } from '@/components';
+import { GradientText } from '@/app/home/components';
+import { section, title, subTitle, button } from './HeroSection.css';
+
+const HeroSection = () => {
+  return (
+    <section className={section}>
+      <Typography variant="display2" as="h1" className={title}>
+        <GradientText color="pinkBlue" text="매일 똑같은 " />
+        <GradientText color="blue" text="삼성 생활" />
+        <GradientText color="pink" text="을" />
+        <br />
+        <GradientText color="pinkBlue" text="더 다채롭게 만들어 줄 " />
+        <GradientText color="blue" text="먹팟" />
+      </Typography>
+      <Typography variant="body1" as="h2" className={subTitle}>
+        우리 회사 동료들과 점심시간 이용해
+        <br />
+        가볍게 대화해보세요!
+      </Typography>
+
+      <Link href="/">
+        <Button color="primary" className={button}>
+          <Typography color="white" variant="title2">
+            우리 회사 먹팟 만들기
+          </Typography>
+        </Button>
+      </Link>
+    </section>
+  );
+};
+
+export default HeroSection;
