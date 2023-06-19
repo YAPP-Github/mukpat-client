c66a900c2c0ec14d367f0a67173f47339f4b0ef2
Yangseungchan <uvzone@naver.com>
2023-06-19 17:30:47 +0900

    chore: Chip 컴포넌트 props 및 타입 정리

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -1,35 +1,22 @@
 import { HTMLAttributes, ReactNode } from 'react';
 import clsx from 'classnames';
-import { wrapper, content, ChipWrapperVariants, ChipContentVariants } from './Chip.css';
+import { wrapper, content, Color, Size } from './Chip.css';
 
-type DivElement = Omit<HTMLAttributes<HTMLDivElement>, 'className'>;
-type ChipVariants = ChipWrapperVariants & ChipContentVariants & DivElement;
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  color?: Color;
+  size?: Size;
+  leftIcon?: ReactNode;
+  rightIcon?: ReactNode;
+}
 
-type Props<Variants extends HTMLAttributes<HTMLDivElement>> = {
-	[key in keyof Variants]: Variants[key];
-} & {
-	children: ReactNode;
-	leftIcon?: ReactNode;
-	rightIcon?: ReactNode;
-	className?: string;
-};
-
-const Chip = ({
-	color = 'grey',
-	size = 'md',
-	children,
-	leftIcon,
-	rightIcon,
-	className = '',
-	...rest
-}: Props<ChipVariants>) => {
-	return (
-		<div className={clsx(wrapper({ color }), className)} {...rest}>
-			{leftIcon}
-			<span className={content({ size })}>{children}</span>
-			{rightIcon}
-		</div>
-	);
+const Chip = ({ color = 'grey', size = 'md', children, leftIcon, rightIcon, className = '', ...rest }: Props) => {
+  return (
+    <div className={clsx(wrapper({ color }), className)} {...rest}>
+      {leftIcon}
+      <span className={content({ size })}>{children}</span>
+      {rightIcon}
+    </div>
+  );
 };
 
 export default Chip;
