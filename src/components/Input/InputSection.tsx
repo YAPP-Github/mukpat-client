fbe44036e9aae789da9d963472f70bfd963c539a
naro_Kim <gracias9022@gmail.com>
2023-06-24 14:43:23 +0900

    feat: inputSection variant, as props 설정 추가

--- src/components/Input/InputSection.tsx
+++ src/components/Input/InputSection.tsx
@@ -2,19 +2,27 @@ import clsx from 'classnames';
 import { ReactNode } from 'react';
 import { section, Direction } from './Input.css';
 import Typography from '../Typography/Typography';
+import { type Variant, type Color } from '../Typography/Typography.css';
 
-type sectionProps = { required?: boolean; label?: string; direction?: Direction; children: ReactNode };
+type sectionProps = {
+  required?: boolean;
+  label?: string;
+  direction?: Direction;
+  children: ReactNode;
+  variant?: Variant;
+  color?: Color;
+  as?: 'code' | 'div' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'label' | 'p' | 'span';
+};
 
-const InputSection = ({ required, children, label, direction }: sectionProps) => {
-	const variant = direction === 'column' ? 'title3' : 'label3';
-	return (
-		<div className={clsx(section({ direction }))}>
-			<Typography required={required} variant={variant} color="secondary" as="label">
-				{label}
-			</Typography>
-			{children}
-		</div>
-	);
+const InputSection = ({ required, variant, children, color = 'primary', as = 'p', label, direction }: sectionProps) => {
+  return (
+    <div className={clsx(section({ direction }))}>
+      <Typography required={required} variant={variant} color={color} as={as}>
+        {label}
+      </Typography>
+      {children}
+    </div>
+  );
 };
 
 export default InputSection;
