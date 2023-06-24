91456b48b8bdd80bf186dfc0d74fa1212fcd95ba
zieun <jieunpark247@gmail.com>
2023-06-24 16:11:14 +0900

    feat: LoginTitle 컴포넌트 추가

---
+++ src/app/login/components/LoginTitle/LoginTitle.css.ts
@@ -0,0 +1,8 @@
+import { style } from '@vanilla-extract/css';
+
+export const title = style({
+  background: 'linear-gradient(90deg, #B0A4EE 0%, #738CF7 100%)',
+  WebkitBackgroundClip: 'text',
+  WebkitTextFillColor: 'transparent',
+  marginTop: '80px',
+});
