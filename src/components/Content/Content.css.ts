bfcc2b864c09c815c99979ae1660c830795c4e40
Yangseungchan <uvzone@naver.com>
2023-06-28 20:15:48 +0900

    refactor: Content 수직 정렬 옵션 추가

--- src/components/Content/Content.css.ts
+++ src/components/Content/Content.css.ts
@@ -1,9 +1,24 @@
-import { style } from '@vanilla-extract/css';
+import { recipe, type RecipeVariants } from '@vanilla-extract/recipes';
 
-export const wrapper = style({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  minHeight: '100vh',
-  justifyContent: 'center',
+export const wrapper = recipe({
+  base: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    minHeight: '100vh',
+  },
+  variants: {
+    verticalCenter: {
+      true: {
+        justifyContent: 'center',
+      },
+      false: {},
+    },
+  },
+  defaultVariants: {
+    verticalCenter: true,
+  },
 });
+
+type WrapperVariant = RecipeVariants<typeof wrapper>;
+export type VerticalCenter = NonNullable<WrapperVariant>['verticalCenter'];
