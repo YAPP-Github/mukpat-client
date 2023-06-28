6711c697597273df07d686f70e67138dad485243
naro_Kim <gracias9022@gmail.com>
2023-06-28 21:32:51 +0900

    chore: SectionProps 수정

--- src/components/Input/InputSection.tsx
+++ src/components/Input/InputSection.tsx
@@ -1,20 +1,14 @@
 import clsx from 'classnames';
-import { ReactNode } from 'react';
+import { ComponentProps } from 'react';
 import { section, Direction } from './Input.css';
 import Typography from '../Typography/Typography';
-import { type Variant, type Color } from '../Typography/Typography.css';
 
-type sectionProps = {
-  required?: boolean;
-  label?: string;
-  direction?: Direction;
-  children: ReactNode;
-  variant?: Variant;
-  color?: Color;
-  as?: 'code' | 'div' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'label' | 'p' | 'span';
-};
+interface SectionProps extends ComponentProps<typeof Typography> {
+  label: string;
+  direction: Direction;
+}
 
-const InputSection = ({ required, variant, children, color = 'primary', as = 'p', label, direction }: sectionProps) => {
+const InputSection = ({ required, variant, children, color = 'primary', as = 'p', label, direction }: SectionProps) => {
   return (
     <div className={clsx(section({ direction }))}>
       <Typography required={required} variant={variant} color={color} as={as}>
