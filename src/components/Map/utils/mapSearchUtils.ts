f35a6f00f0d4c5ae2bd568deeb3dbaa04352fcba
zieun <jieunpark247@gmail.com>
2023-07-01 02:39:31 +0900

    edit: 반경 1km 기준으로 찾는 로직 삭제

--- src/components/Map/utils/mapSearchUtils.ts
+++ src/components/Map/utils/mapSearchUtils.ts
@@ -1,19 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { PlaceList } from '../types';
-import { creatPlaces, getCurrentCoordinate } from './mapUtils';
+import { creatPlaces } from './mapUtils';
 
-export const filteredOptions = async () => {
-  const currentCoordinate = await getCurrentCoordinate();
-  const options = {
-    location: currentCoordinate,
-    radius: 10000,
-    sort: window.kakao.maps.services.SortBy.DISTANCE,
-  };
-  return options;
-};
 export const searchPlaces = async (keyword: string): Promise<PlaceList> => {
   const ps = creatPlaces();
-  const options = await filteredOptions();
   let places: PlaceList = [];
 
   const placesSearchCB = new Promise<PlaceList>((resolve, reject) => {
@@ -31,7 +21,7 @@ export const searchPlaces = async (keyword: string): Promise<PlaceList> => {
         reject('장소 검색 중 오류가 발생했습니다.');
       }
     };
-    ps.keywordSearch(keyword, searchCallback, options);
+    ps.keywordSearch(keyword, searchCallback);
   });
 
   return placesSearchCB;
