ea545d8866dc49ae16fe6a7f5c3414618d7781d5
zieun <jieunpark247@gmail.com>
2023-07-01 10:42:08 +0900

    edit: input 창 커스텀하여 width 조정

--- src/app/signup/components/InputArea/InputArea.tsx
+++ src/app/signup/components/InputArea/InputArea.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { Input, InputSection } from '@/components';
 import { FieldValues, UseFormReturn } from 'react-hook-form';
-import { inputWrapper } from './InputArea.css';
+import { inputWrapper, input } from './InputArea.css';
 import { HTMLAttributes } from 'react';
 import cx from 'classnames';
 
@@ -18,7 +18,14 @@ const InputArea = ({ label, name, placeholder, required, method, type, className
   return (
     <div className={cx(inputWrapper, className)}>
       <InputSection label={label} variant="label3" color="sub" direction="column" required={required}>
-        <Input {...method.register(name)} name={name} placeholder={placeholder} showError={true} type={type} />
+        <Input
+          {...method.register(name)}
+          name={name}
+          placeholder={placeholder}
+          showError={true}
+          type={type}
+          className={input}
+        />
       </InputSection>
     </div>
   );
