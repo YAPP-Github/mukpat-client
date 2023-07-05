baa579c030e924d31544d06a27d55e66cc689cbf
Yangseungchan <uvzone@naver.com>
2023-07-06 00:04:15 +0900

    feat: 모집 상태에 카드 음영처리

--- src/app/home/components/BoardCard/BoardCard.tsx
+++ src/app/home/components/BoardCard/BoardCard.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { Chip, Profile, SvgIcon, Typography } from '@/components';
 import { BoardListItem } from '@/api/types';
+import { RECRUIT_STATUS } from '@/app/home/constants';
 import { getChipColor } from '@/app/home/utils/chipColor';
 
 import {
@@ -34,59 +35,67 @@ const BoardCard = ({ boardListItem }: Props) => {
     participants,
   } = boardListItem;
 
+  const isRecruiting = status === RECRUIT_STATUS.RECRUITING;
+
   return (
-    <Link href={`/board/${boardId}`}>
-      <li className={wrapper}>
-        <div className={header}>
-          <div className={chips}>
-            <Chip color={getChipColor(status)}>{status}</Chip>
-            {todayOrTomorrow && <Chip color={getChipColor(todayOrTomorrow)}>{todayOrTomorrow}</Chip>}
+    <li>
+      <Link href={`/board/${boardId}`}>
+        <div
+          className={wrapper({
+            isRecruiting,
+          })}
+        >
+          <div className={header}>
+            <div className={chips}>
+              <Chip color={getChipColor(status)}>{status}</Chip>
+              {todayOrTomorrow && <Chip color={getChipColor(todayOrTomorrow)}>{todayOrTomorrow}</Chip>}
+            </div>
+            <Typography variant="label2" color="sub">
+              {elapsedTime}
+            </Typography>
           </div>
-          <Typography variant="label2" color="sub">
-            {elapsedTime}
+          <Typography className={titleStyle} variant="title1" color="primary">
+            {title}
           </Typography>
-        </div>
-        <Typography className={titleStyle} variant="title1" color="primary">
-          {title}
-        </Typography>
-        <div className={body}>
-          <div className={bodyItem}>
-            <SvgIcon id="calendar" color="grey300" width={24} height={24} />
-            <Typography variant="body2" color="primary" className={bodyItemText}>
-              {meetingDateTime}
-            </Typography>
+          <div className={body}>
+            <div className={bodyItem}>
+              <SvgIcon id="calendar" color="grey300" width={24} height={24} />
+              <Typography variant="body2" color="primary" className={bodyItemText}>
+                {meetingDateTime}
+              </Typography>
+            </div>
+            <div className={bodyItem}>
+              <SvgIcon id="map" color="grey300" width={24} height={24} />
+              <Typography variant="body2" color="primary" className={bodyItemText}>
+                {meetingPlace}
+              </Typography>
+            </div>
           </div>
-          <div className={bodyItem}>
-            <SvgIcon id="map" color="grey300" width={24} height={24} />
-            <Typography variant="body2" color="primary" className={bodyItemText}>
-              {meetingPlace}
-            </Typography>
-          </div>
-        </div>
-        <div className={footer}>
-          <div className={participantsCount}>
-            <Typography color="hint" variant="label3">
-              참여 멤버
-            </Typography>
-            <Typography color="sub" variant="label3">
-              {currentApply}/{maxApply}
-            </Typography>
-          </div>
-          <div className={participantsList}>
-            {participants.map(({ userId, nickName }, idx) => (
-              <Profile
-                key={idx}
-                uid={userId}
-                nickname={nickName}
-                {...(!userId && {
-                  fontSize: 'small',
-                })}
-              />
-            ))}
+          <div className={footer}>
+            <div className={participantsCount}>
+              <Typography color="hint" variant="label3">
+                참여 멤버
+              </Typography>
+              <Typography color="sub" variant="label3">
+                {currentApply}/{maxApply}
+              </Typography>
+            </div>
+            <div className={participantsList}>
+              {participants.map(({ userId, nickName }, idx) => (
+                <Profile
+                  key={idx}
+                  uid={userId}
+                  nickname={nickName}
+                  {...(!userId && {
+                    fontSize: 'small',
+                  })}
+                />
+              ))}
+            </div>
           </div>
         </div>
-      </li>
-    </Link>
+      </Link>
+    </li>
   );
 };
 
