f7c3ea15270b19cfca6dd98400d4053196d606f1
zieun <jieunpark247@gmail.com>
2023-07-08 15:51:24 +0900

    fix: vite config 설정 하나로 통일

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,19 @@
 import { defineConfig, mergeConfig } from 'vitest/config';
-import viteConfig from './vite.config';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 // https://vitejs.dev/config/
-export default mergeConfig(
-	viteConfig,
-	defineConfig({
-		test: {
-			globals: true,
-			environment: 'jsdom',
-			setupFiles: ['./src/tests/setupTests.ts'],
-			maxThreads: 15,
-			minThreads: 8,
-		},
-	}),
-);
+export default defineConfig({
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: ['./src/tests/setupTests.ts'],
+		maxThreads: 15,
+		minThreads: 8,
+	},
+	plugins: [react(), vanillaExtractPlugin()],
+	resolve: {
+		alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+	},
+});
