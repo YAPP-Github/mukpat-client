e50809445c0837986dc963ecb6b945f00c2758ee
Yangseungchan <uvzone@naver.com>
2023-07-08 22:49:36 +0900

    chore: vitest 버전 호환 관련 오류 해결

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig, mergeConfig } from 'vitest/config';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	test: {
-		globals: true,
-		environment: 'jsdom',
-		setupFiles: ['./src/tests/setupTests.ts'],
-		maxThreads: 15,
-		minThreads: 8,
-	},
-	plugins: [react(), vanillaExtractPlugin()],
-	resolve: {
-		alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
-	},
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/tests/setupTests.ts'],
+    maxThreads: 15,
+    minThreads: 8,
+  },
+  plugins: [react(), vanillaExtractPlugin()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
 });
