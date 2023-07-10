ea170cbc6a9dd408d1a9dad5662b0352175f9ca6
Yangseungchan <uvzone@naver.com>
2023-07-10 21:36:23 +0900

    feat: useClickOutside 예외케이스 추가

--- src/hooks/useClickOutside/useClickOutside.test.tsx
+++ src/hooks/useClickOutside/useClickOutside.test.tsx
@@ -4,32 +4,41 @@ import useClickOutside from './useClickOutside';
 import userEvent from '@testing-library/user-event';
 
 const TestComponent = ({ handleClickOutside }: { handleClickOutside: () => void }) => {
-	const ref = useClickOutside<HTMLDivElement>({
-		onClickOutside: handleClickOutside,
-	});
+  const ref = useClickOutside<HTMLDivElement>({
+    onClickOutside: handleClickOutside,
+  });
 
-	return (
-		<div>
-			<div ref={ref}>target</div>
-			<div>nontarget</div>
-		</div>
-	);
+  return (
+    <div>
+      <div id="overlay-container">overlay</div>
+      <div ref={ref}>target</div>
+      <div>nontarget</div>
+    </div>
+  );
 };
 
 describe('useClickOutside 테스트', () => {
-	test('ref로 지정한 영역 바깥 부분을 누를때 이벤트가 감지된다', async () => {
-		const handleClickOutside = vi.fn();
-		render(<TestComponent handleClickOutside={handleClickOutside} />);
-		const nonTarget = screen.getByText('nontarget');
-		await userEvent.click(nonTarget);
-		expect(handleClickOutside).toHaveBeenCalled();
-	});
+  test('ref로 지정한 영역 바깥 부분을 누를때 이벤트가 감지된다', async () => {
+    const handleClickOutside = vi.fn();
+    render(<TestComponent handleClickOutside={handleClickOutside} />);
+    const nonTarget = screen.getByText('nontarget');
+    await userEvent.click(nonTarget);
+    expect(handleClickOutside).toHaveBeenCalled();
+  });
 
-	test('ref로 지정한 영역을 누르면 이벤트가 감지되지 않는다', async () => {
-		const handleClickOutside = vi.fn();
-		render(<TestComponent handleClickOutside={handleClickOutside} />);
-		const target = screen.getByText('target');
-		await userEvent.click(target);
-		expect(handleClickOutside).not.toHaveBeenCalled();
-	});
+  test('ref로 지정한 영역을 누르면 이벤트가 감지되지 않는다', async () => {
+    const handleClickOutside = vi.fn();
+    render(<TestComponent handleClickOutside={handleClickOutside} />);
+    const target = screen.getByText('target');
+    await userEvent.click(target);
+    expect(handleClickOutside).not.toHaveBeenCalled();
+  });
+
+  test('ref로 지정한 대상이 overlay-container에 없는 상황에서 클릭 대상이 overlay-container에 있으면 이벤트는 감지되지 않는다', async () => {
+    const handleClickOutside = vi.fn();
+    render(<TestComponent handleClickOutside={handleClickOutside} />);
+    const target = screen.getByText('overlay');
+    await userEvent.click(target);
+    expect(handleClickOutside).not.toHaveBeenCalled();
+  });
 });
