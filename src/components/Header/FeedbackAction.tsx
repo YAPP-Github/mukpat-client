be2fdd0513bbdc6f68448d47265fa2f773cf6e7d
Yangseungchan <uvzone@naver.com>
2023-07-12 10:15:50 +0900

    chore: feedback 아이콘 변경

--- src/components/Header/FeedbackAction.tsx
+++ src/components/Header/FeedbackAction.tsx
@@ -4,7 +4,7 @@ import { feedbackAction } from './Header.css';
 const FeedbackAction = () => {
   return (
     <a className={feedbackAction} href="https://forms.gle/mgNnVijeG5YgQL9R8" target="_blank">
-      <SvgIcon id="chat" width={18} height={18} />
+      <SvgIcon id="feedback" width={18} height={18} />
       <Typography variant="label3" color="secondary">
         피드백
       </Typography>
