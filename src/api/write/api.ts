a0c15866baa82831b009d85fc1e59e18eef58e79
naro-Kim <gracias9022@gmail.com>
2023-07-14 22:22:04 +0900

    feat: post api mutation query 변경

---
+++ src/api/write/api.ts
@@ -0,0 +1,34 @@
+import { request } from '@/utils/ky/request';
+import { ParsedData } from '@/app/write/types';
+class WriteAPI {
+  /**
+   * 유저가 입력한 데이터로 먹팟을 생성합니다.
+   * @param data - 유저가 입력한 board 데이터
+   */
+  async postBoard({ ...data }: ParsedData) {
+    return request
+      .post('v2/boards', {
+        json: {
+          ...data,
+        },
+      })
+      .json();
+  }
+  /**
+   * boardId에 해당하는 먹팟을 수정합니다
+   * @param boardId - 수정할 board의 id
+   * @param data - 유저가 입력한 board 데이터
+   */
+  async patchBoard(boardId: number, { ...data }: ParsedData) {
+    return request
+      .patch(`v2/boards/${boardId}`, {
+        json: {
+          ...data,
+        },
+      })
+      .json();
+  }
+}
+
+// api fetchers instance
+export const writeAPI = new WriteAPI();
--- src/app/write/components/index.ts
+++ src/app/write/components/index.ts
@@ -1,7 +1,9 @@
 export { default as AgeModal } from './AgeModal/AgeModal';
+export { default as AgeBottomSheet } from './AgeModal/AgeBottomSheet';
 export { default as Counter } from './Counter/Counter';
 export { default as InputDate } from './InputDate/InputDate';
 export { default as FirstStep } from './Form/FirstStep';
 export { default as SecondStep } from './Form/SecondStep';
 export { default as MapModal } from './MapModal/MapModal';
 export { default as WriteTitle } from './WriteTitle/WriteTitle';
+export { default as TimeDropDown } from './TimeDropDown/TimeDropDown';
