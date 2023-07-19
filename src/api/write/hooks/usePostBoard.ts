60441c352281a9883b348ff10415c229531f7832
naro_Kim <gracias9022@gmail.com>
2023-07-20 02:23:25 +0900

    chore: useFunnel, usePostBoard 수정

--- src/api/write/hooks/usePostBoard.ts
+++ src/api/write/hooks/usePostBoard.ts
@@ -2,13 +2,11 @@ import { useQueryClient } from '@tanstack/react-query';
 import { useMutation } from '@/hooks';
 import { boardKeys } from '@/api/board';
 import { writeAPI } from '@/api/write/api';
-import { ParsedData, PostResponse } from '@/app/write/types';
-import { HTTPError } from 'ky';
 
 const usePostBoard = () => {
   const queryClient = useQueryClient();
 
-  return useMutation<PostResponse, HTTPError, ParsedData, unknown>(writeAPI.postBoard, {
+  return useMutation(writeAPI.postBoard, {
     onSuccess: () => {
       void queryClient.invalidateQueries(boardKeys.list());
     },
