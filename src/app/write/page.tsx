2cdd904b74918c2e39c248b5091487fc9a0dd47b
naro_Kim <gracias9022@gmail.com>
2023-07-22 11:00:25 +0900

    feat: 새로고침 및 비로그인 방지

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -2,17 +2,20 @@
 import FirstStep from './components/Form/FirstStep';
 import SecondStep from './components/Form/SecondStep';
 import { wrapper } from './style.css';
-import { useEffect } from 'react';
 import { useFunnel } from '@/hooks';
 import { useProfile } from '@/api/hooks';
 import WriteTitle from './components/WriteTitle/WriteTitle';
+import useFormStore from './store/useFormStore';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Write() {
   const [step, { prevStep, nextStep }] = useFunnel(['1', '2']);
-  const { data: profile } = useProfile();
-  const isLogin = Boolean(profile);
-  if (!isLogin) {
-    // window.history.back();
+  const { reset, setData } = useFormStore();
+  const { data } = useProfile();
+  const router = useRouter();
+  if (!data) {
+    router.push('/login');
   }
 
   const preventClose = (e: BeforeUnloadEvent) => {
@@ -21,19 +24,18 @@ export default function Write() {
   };
 
   useEffect(() => {
-    (() => {
-      window.addEventListener('beforeunload', preventClose);
-    })();
+    window.addEventListener('beforeunload', preventClose);
     return () => {
       window.removeEventListener('beforeunload', preventClose);
+      reset();
     };
   }, []);
 
   return (
     <div className={wrapper}>
       <WriteTitle prevStep={prevStep} />
-      {step === '1' && <FirstStep nextStep={nextStep} />}
-      {step === '2' && <SecondStep />}
+      {step === '1' && <FirstStep setData={setData} nextStep={nextStep} />}
+      {step === '2' && <SecondStep reset={reset} />}
     </div>
   );
 }
