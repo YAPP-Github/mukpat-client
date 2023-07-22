73900508048b4638b132f3b5f9e2eb1672636dd1
naro_Kim <gracias9022@gmail.com>
2023-07-22 17:20:14 +0900

    fix: 코드 중복 제거

--- src/app/write/components/InputLoading/InputLoading.tsx
+++ src/app/write/components/InputLoading/InputLoading.tsx
@@ -2,43 +2,29 @@ import { Skeleton } from '@/components';
 import { loadingForm, row, section, wrapper } from './InputLoading.css';
 
 const InputLoading = () => {
+  const SkeletonInput = () => {
+    return (
+      <div className={wrapper}>
+        <Skeleton width="140px" height="28px" mb="20px" />
+        {Array(2)
+          .fill(0)
+          .map((_, index) => (
+            <div className={row} key={index}>
+              <Skeleton width="64px" height="24px" />
+              <Skeleton width="505px" height="56px" />
+            </div>
+          ))}
+      </div>
+    );
+  };
+
   return (
     <div className={loadingForm}>
       <div className={section}>
         <Skeleton width="324px" height="42px" />
-        <div className={wrapper}>
-          <Skeleton width="140px" height="28px" mb="20px" />
-          {Array(2)
-            .fill(0)
-            .map((_, index) => (
-              <div className={row} key={index}>
-                <Skeleton width="64px" height="24px" />
-                <Skeleton width="505px" height="56px" />
-              </div>
-            ))}
-        </div>
-        <div className={wrapper}>
-          <Skeleton width="140px" height="28px" mb="20px" />
-          {Array(2)
-            .fill(0)
-            .map((_, index) => (
-              <div className={row} key={index}>
-                <Skeleton width="64px" height="24px" />
-                <Skeleton width="505px" height="56px" />
-              </div>
-            ))}
-        </div>
-        <div className={wrapper}>
-          <Skeleton width="140px" height="28px" mb="20px" />
-          {Array(2)
-            .fill(0)
-            .map((_, index) => (
-              <div className={row} key={index}>
-                <Skeleton width="64px" height="24px" />
-                <Skeleton width="505px" height="56px" />
-              </div>
-            ))}
-        </div>
+        <SkeletonInput />
+        <SkeletonInput />
+        <SkeletonInput />
       </div>
       <div>
         <Skeleton mt="64px" width="674px" height="54px" radius="md" />
