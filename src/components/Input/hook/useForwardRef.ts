4a5f5c9e3b62df3fd63965e8a7533b864a50e75c
naro_Kim <gracias9022@gmail.com>
2023-07-22 22:52:03 +0900

    chore: eslint any issue 수정

--- src/components/Input/hook/useForwardRef.ts
+++ src/components/Input/hook/useForwardRef.ts
@@ -1,19 +1,20 @@
 import { useRef, useEffect, ForwardedRef } from 'react';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const useForwardRef = <T>(ref: ForwardedRef<T>, initialValue: any = null) => {
-	const targetRef = useRef<T>(initialValue);
+  const targetRef = useRef<T>(initialValue);
 
-	useEffect(() => {
-		if (!ref) return;
+  useEffect(() => {
+    if (!ref) return;
 
-		if (typeof ref === 'function') {
-			ref(targetRef.current);
-		} else {
-			ref.current = targetRef.current;
-		}
-	}, [ref]);
+    if (typeof ref === 'function') {
+      ref(targetRef.current);
+    } else {
+      ref.current = targetRef.current;
+    }
+  }, [ref]);
 
-	return targetRef;
+  return targetRef;
 };
 
 export default useForwardRef;
