4ab4345d11609a689f6c415809aa3954ec547f6d
Yangseungchan <uvzone@naver.com>
2023-07-28 17:16:28 +0900

    feat: 로고 클릭시 지역 필터 초기화

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,9 +1,9 @@
-import Link from 'next/link';
+import LogoLink from './LogoLink';
 import { logo } from './Logo.css';
 
 const Logo = () => {
   return (
-    <Link href="/">
+    <LogoLink>
       <div className={logo}>
         <svg width="100%" height="100%" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path
@@ -33,7 +33,7 @@ const Logo = () => {
           </defs>
         </svg>
       </div>
-    </Link>
+    </LogoLink>
   );
 };
 
