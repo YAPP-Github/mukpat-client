200bf81eae4f490fbf5491ec85acc99abfcf00e8
Yangseungchan <uvzone@naver.com>
2023-07-30 14:54:07 +0900

    feat: AsideSection에 정책 반영

--- src/app/board/components/AsideSection/AsideSection.tsx
+++ src/app/board/components/AsideSection/AsideSection.tsx
@@ -2,10 +2,11 @@
 
 import { Button, Typography, Toast } from '@/components';
 import { JoinModal, CancelJoinModal, ParticipantsList, DetailMenuButton } from '@/app/board/components';
-import { useProfile } from '@/api/hooks';
+import { useChangeBoardStatus, useProfile } from '@/api/hooks';
+import { useBoardStates } from '@/app/board/hooks';
 import { useOverlay, useLoginRedirect } from '@/hooks';
-import { BOARD_STATUS, TOAST_TEXT } from '@/app/board/constants';
-import { BoardDetail } from '@/api/types';
+import { BOARD_STATUS, BoardDetail } from '@/api/types';
+import { TOAST_TEXT } from '@/app/board/constants';
 import { wrapper, listBottomSpace, buttonGroup } from './AsideSection.css';
 
 interface Props {
@@ -13,16 +14,15 @@ interface Props {
 }
 
 const AsideSection = ({ board }: Props) => {
+  const { boardId, chatLink } = board;
+
   const { data: profile } = useProfile();
   const { redirectToLogin } = useLoginRedirect();
   const [openModal, closeModal] = useOverlay();
   const [openToast, closeToast] = useOverlay();
-
-  const { boardId, chatLink, participants, status, minAge, maxAge, userAge } = board;
-
-  const isJoined = participants.find(({ userId }) => userId === profile?.userId);
-
-  const isNotPossibleAge = Boolean(userAge && minAge && maxAge) && !(minAge <= userAge && userAge <= maxAge);
+  const { isJoined, isJoinCancellable, isJoinable, isPossibleAge, isClosable, isAgainRecruitable } =
+    useBoardStates(board);
+  const { mutate: changeBoardStatus } = useChangeBoardStatus(boardId);
 
   const handleClickJoinButton = () => {
     if (!profile) return redirectToLogin();
@@ -58,6 +58,28 @@ const AsideSection = ({ board }: Props) => {
     );
   };
 
+  const handleClickCloseButton = () => {
+    changeBoardStatus(BOARD_STATUS.DONE, {
+      onSuccess: () => {
+        openToast(<Toast type="success" message={TOAST_TEXT.SUCCESS_MAKE_DONE} onClose={closeToast} />);
+      },
+      onError: (err) => {
+        openToast(<Toast type="warn" message={err.message} onClose={closeToast} />);
+      },
+    });
+  };
+
+  const handleClickReopenButton = () => {
+    changeBoardStatus(BOARD_STATUS.IN_PROGRESS, {
+      onSuccess: () => {
+        openToast(<Toast type="success" message={TOAST_TEXT.SUCCESS_MAKE_INPROGRESS} onClose={closeToast} />);
+      },
+      onError: (err) => {
+        openToast(<Toast type="warn" message={err.message} onClose={closeToast} />);
+      },
+    });
+  };
+
   return (
     <aside className={wrapper}>
       <DetailMenuButton board={board} />
@@ -65,14 +87,10 @@ const AsideSection = ({ board }: Props) => {
       <div className={buttonGroup}>
         {!isJoined && (
           <>
-            <Button
-              size="medium"
-              disabled={status === BOARD_STATUS.DONE || isNotPossibleAge}
-              onClick={handleClickJoinButton}
-            >
+            <Button size="medium" disabled={!isJoinable} onClick={handleClickJoinButton}>
               참여하기
             </Button>
-            {isNotPossibleAge && (
+            {!isPossibleAge && (
               <Typography variant="label3" as="p" color="red500">
                 나이 제한이 있는 먹팟이에요.
               </Typography>
@@ -84,9 +102,21 @@ const AsideSection = ({ board }: Props) => {
             <Button size="medium" onClick={handleClickOpenChatButton}>
               오픈 채팅방 바로가기
             </Button>
-            <Button size="medium" color="text" onClick={handleClickCancelJoinButton}>
-              참가 취소하기
-            </Button>
+            {isJoinCancellable && (
+              <Button size="medium" color="text" onClick={handleClickCancelJoinButton}>
+                참가 취소하기
+              </Button>
+            )}
+            {isClosable && (
+              <Button size="medium" color="text" onClick={handleClickCloseButton}>
+                모집 마감하기
+              </Button>
+            )}
+            {isAgainRecruitable && (
+              <Button size="medium" color="text" onClick={handleClickReopenButton}>
+                다시 모집하기
+              </Button>
+            )}
           </>
         )}
       </div>
