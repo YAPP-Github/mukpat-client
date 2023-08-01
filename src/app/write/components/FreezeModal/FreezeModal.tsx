8534f6fcd3fefc7485496aa4f9700a766638ccb9
naro_Kim <gracias9022@gmail.com>
2023-08-01 18:46:52 +0900

    feat: 이탈 방지 FreezeModal 컴포넌트 생성

--- src/app/write/components/FreezeModal/FreezeModal.tsx
+++ src/app/write/components/FreezeModal/FreezeModal.tsx
@@ -2,24 +2,31 @@ import { Button, Modal } from '@/components';
 
 interface Props {
   onClose: () => void;
-  onClick: () => void;
+  onClick?: () => void;
+  content?: string;
+  footer?: string | string[];
 }
 
-const PreventModal = ({ onClose, onClick }: Props) => {
+const FreezeModal = ({
+  content = '페이지를 나가면 작성 중인 먹팟이 삭제돼요.',
+  footer = ['나가기', '계속 작성하기'],
+  onClose,
+  onClick,
+}: Props) => {
   return (
     <Modal onClose={onClose} size="small">
       <Modal.Header type="info" title="안내" />
-      <Modal.Content size="small">페이지를 나가면 작성 중인 먹팟이 삭제돼요. </Modal.Content>
+      <Modal.Content size="small">{content}</Modal.Content>
       <Modal.Footer type="horizontal">
         <Button onClick={onClick} color="text" size="micro">
-          나가기
+          {footer[0]}
         </Button>
         <Button onClick={onClose} color="primary" size="micro">
-          계속 작성하기
+          {footer[1]}
         </Button>
       </Modal.Footer>
     </Modal>
   );
 };
 
-export default PreventModal;
+export default FreezeModal;
